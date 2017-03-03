83e014fbbc6432aa4edd54155ec93197b37e349f
babel <kamase79@daum.net>
2017-03-03 17:25:12 +0900

    remove suffix word.

--- core/layout.el
+++
@@ -1,9 +0,0 @@
-;;+-----------------------~------------------------~---------------------+
-;;|                        Emacs's ecb custom layout                     |
-;;+-----------------------~------------------------~---------------------+
-(ecb-layout-define "babel-custom-speedbar" left
-  "This function creates the following layout by babel"
-  (ecb-set-speedbar-buffer)
-  (select-window (next-window)))
-
-(provide 'layout)
