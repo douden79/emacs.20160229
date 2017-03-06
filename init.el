4a4318155d4c7273f6c38c7238ee7d817f45e583
babel <kamase79@daum.net>
2017-03-06 11:22:58 +0900

    remove error point file. 	수정함:        init.el

--- init.el
+++ init.el
@@ -27,7 +27,21 @@
 (require 'core-editor)
 (require 'core-keybindings)
 (require 'core-autocomplete)
-(require 'core-c++)
 (require 'core-ide)
 (require 'core-error)
-(require 'core-python-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-gtags-auto-update t)
+ '(helm-gtags-ignore-case t)
+ '(helm-gtags-path-style (quote relative))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Envy Code R" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
