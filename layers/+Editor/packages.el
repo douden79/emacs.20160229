dc2011a9168c004a3e47bd214e8bc8f73d5168b9
babel <kamase79@daum.net>
2017-04-04 11:24:45 +0900

    add editor package.

--- layers/+Editor/packages.el
+++ layers/+Editor/packages.el
@@ -8,9 +8,55 @@
 
 ;; ▶ General
 ;; ▼ CodeComplete ( Autocomplete, yasnippet )
+(defun editor/autocomplete ()
+  "autocomplete init"
+  (use-package auto-complete
+    :ensure t
+    :init (ac-config-default)
+    (global-auto-complete-mode t)
+    (setq ac-auto-start 1)
+    (setq ac-auto-show-menu 0.1)
+    (ac-set-trigger-key "TAB"))
+  )
+
+;; ▼ yasnippet
+(defun editor/yasnippet ()
+  "yasnippet init"
+  (use-package yasnippet
+    :ensure t
+    :defer t
+    :diminish yas-minor-mode
+    :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
+    :init
+    (progn
+      (setq yas-verbosity 3)
+      (yas-global-mode 1))
+    (add-hook 'term-mode-hook (lambda() (setq yas-dont-activate t)))
+    (define-key yas-minor-mode-map (kbd "<tab>") nil)
+    (define-key yas-minor-mode-map (kbd "TAB") nil)
+    (define-key yas-minor-mode-map (kbd "C-c y") 'yas-expand)
+    (ac-set-trigger-key "TAB")
+    (ac-set-trigger-key "<tab>")
+    (add-hook
+     'prog-mode-hook
+     (lambda ()
+       (setq ac-sources
+             (append '(ac-source-yasnippet) ac-sources)))))
+  )
+
 ;; ▼ Appearance
-;; ▼ Editor tabs
+;; ▼ Editor modify
 ;; ▼ code folding
+(defun editor/hideshowvis ()
+  "hideshowvis init"
+  (use-package hideshowvis
+    :ensure t
+    :bind (("C-t" . hs-toggle-hiding))
+    :init (add-hook 'c-mode-hook #'hideshowvis-enable)
+    (add-hook 'c++-mode-hook #'hideshowvis-enable)
+    (add-hook 'c-mode-hook #'hideshowvis-symbols)
+    (add-hook 'c++-mode-hook #'hideshowvis-symbols))
+  )
 
 ;; ▶ Color / Fonts
 ;; ▶ Code Style
@@ -20,5 +66,13 @@
 ;; Editor init
 (defun editor/init ()
   "Editor envirment init"
+  ;; Default init
   (better/smex)
+  
+  ;; Autocomplete
+  (editor/autocomplete)
+  (editor/yasnippet)
+
+  ;; code folding
+  (editor/hideshowvis)
   )
